bd4bef5609f9872320c0af8ca33369ba67d84d43
Jake Fawcett <jakefawcett2298@hotmail.co.uk>
2022-12-05 10:51:32 +0000

    day 4 solution

---
+++ day4/solution.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"common"
+	"strings"
+	"strconv"
+)
+
+const file_name string = "input.txt"
+
+func extractRanges(line string) (int, int, int, int) {
+	var rangeOne = strings.Split(strings.Split(line, ",")[0], "-")
+	var lowerRangeOne, _ = strconv.Atoi(rangeOne[0])
+	var upperRangeOne, _ = strconv.Atoi(rangeOne[1])
+	var rangeTwo = strings.Split(strings.Split(line, ",")[1], "-")
+	var lowerRangeTwo, _ = strconv.Atoi(rangeTwo[0])
+	var upperRangeTwo, _ = strconv.Atoi(rangeTwo[1])
+	return lowerRangeOne, upperRangeOne, lowerRangeTwo, upperRangeTwo
+}
+
+func partOne(fileLines []string) (int) {
+	var containedRanges int = 0
+	for _, line := range fileLines {
+		var lowerRangeOne, upperRangeOne, lowerRangeTwo, upperRangeTwo int = extractRanges(line)
+		if (lowerRangeOne <= lowerRangeTwo && upperRangeOne >= upperRangeTwo) || (lowerRangeOne >= lowerRangeTwo && upperRangeOne <= upperRangeTwo) {
+			containedRanges += 1
+		} 
+    }
+	return containedRanges
+}
+
+func partTwo(fileLines []string) (int) {
+	var containedRanges int = 0
+	for _, line := range fileLines {
+		var lowerRangeOne, upperRangeOne, lowerRangeTwo, upperRangeTwo int = extractRanges(line)
+		if !(lowerRangeOne > upperRangeTwo || lowerRangeTwo > upperRangeOne) {
+			containedRanges += 1
+		} 
+    }
+	return containedRanges
+}
+
+func main() {
+	fileLines := common.ReadTextFile(file_name)
+	fmt.Println("Part one:", partOne(fileLines))
+	fmt.Println("Part two:", partTwo(fileLines))
+}
