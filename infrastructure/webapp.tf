2368c46e907f6fdcd4bfb5ad47aeed387d11bb38
JakeF <jakefawcett2298@hotmail.co.uk>
2023-02-01 08:12:45 +0000

    removed vars from backend

--- infrastructure/webapp.tf
+++ infrastructure/webapp.tf
@@ -39,9 +39,9 @@ terraform {
     }
   }
   backend "azurerm" {
-    resource_group_name  = var.resource_group_name
-    storage_account_name = var.storage_account_name
-    container_name       = var.storage_account_container_name
+    resource_group_name  = "aoc-2022-dev"
+    storage_account_name = "tfstateaoc2022dev"
+    container_name       = "tfstate"
     key                  = "terraform.tfstate"
   }
 }
