70b93fb10d4721dc1125c3dc0fc2ff4e623f728d
JakeF <jakefawcett2298@hotmail.co.uk>
2023-02-02 23:16:52 +0000

    set app port

--- app/main.go
+++ app/main.go
@@ -41,6 +41,15 @@ func main() {
         http.HandleFunc(path, DayServer(function))
 	}
 
+    // Azure App Service sets the port as an Environment Variable
+	// This can be random, so needs to be loaded at startup
+	port := os.Getenv("HTTP_PLATFORM_PORT")
+
+	// default back to 8080 for local dev
+	if port == "" {
+		port = "8080"
+	}
+
     http.ListenAndServe(":8080", nil)
 }
 
